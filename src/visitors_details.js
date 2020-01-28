7a6dbb2d3ccab5b676a58b78023019488ce1017e
Portia-arch <portia.mkhabela@umuzi.org>
2020-01-28 10:18:27 +0200

    function changes

--- src/visitors_details.js
+++ src/visitors_details.js
@@ -1,6 +1,5 @@
 'use strict';
 const fs = require('fs');
-const load = require('../src/load_details')
 
 class Visitor {
     constructor(Name, Age, Visit_date, Time, Comments, Assistant) {
@@ -17,23 +16,34 @@ class Visitor {
 
         fs.writeFile(`visitor_${name}.json`, JSON.stringify(this, null, 2), function (err) {
             if (err) throw err;
-            //    console.log('Saved!');
+               console.log('Saved!');
         });
         return (this);
     }
 }
 
-let alice = new Visitor('Alice Cooper', 12, '12/03/2019', '13:26', 'yey!', 'Weston')
-alice.save();
-// console.log(alice);
 
-let bob = new Visitor('Bob Marley', 26, '1/06/2020', '08:26', 'Boring', 'Mpumelelo')
-bob.save();
-// console.log(bob); 
+async function load(fullName) {
 
-let charlie = new Visitor('Charlie Sheen', 91, '16/09/2019', '10:15', 'Yeeeeeeeeeeeey!!!!', 'Tadiwa')
-charlie.save();
-// console.log(charlie);
+    const fs = require('fs');
+    let name = fullName.replace(' ', '_').toLowerCase();
+
+    fs.readFile(`visitor_${name}.json`, 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+
+        else {
+            console.log(data);
+        }
+    });
+}
+
+load("Alice Cooper");
+
+//  exports.module = {
+//      load
+//  }
 
 module.exports = {
     Visitor
